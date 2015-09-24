22c9860d1756256e996b593bf50711792f2865ff
Jah Seng Lee <jahseng619@gmail.com>
2015-09-24 12:48:55 +1200

    Container
    New abstract container class. This class will be used for any containers
    which can hold Item objects

---
+++ SWEN222 ECS Wars/src/gameObjects/containers/Backpack.java
@@ -0,0 +1,68 @@
+package gameObjects.containers;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import main.GUICanvas;
+
+/**
+ * A backpack which represents the player's inventory
+ * Backpack should be able to hold 10 items
+ * 
+ * @author Jah Seng Lee
+ *
+ */
+public class Backpack extends Container {
+	
+	public static final int SIZE = 10;
+	
+	public Backpack(){
+		super(SIZE);
+	}
+	
+	@Override
+	public void use() {
+		
+	}
+
+	@Override
+	public Image getImage(int viewDirection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean canWalk() {
+		return true;
+	}
+
+	@Override
+	public int yOffset(int viewDirection) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int xOffset(int viewDirection) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setScaledImage(int viewDirection, Image scaledImage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Image getScaledImage(int viewDirection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void draw(Graphics g, GUICanvas c) {
+		// TODO Auto-generated method stub
+		
+	}
+}
