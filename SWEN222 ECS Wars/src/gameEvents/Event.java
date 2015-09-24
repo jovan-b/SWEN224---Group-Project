ccadb7a79cfff9e514ad3c42c432de4d9d691ca2
Carl <narceil@hotmail.com>
2015-09-24 14:18:43 +1200

    Added a game clock scheduler

---
+++ SWEN222 ECS Wars/src/gameEvents/Event.java
@@ -0,0 +1,20 @@
+package gameEvents;
+
+public interface Event {
+	/**
+	 * Notify the event what the time is, so the event can decide whether to run
+	 * @param time
+	 */
+	public void notify(int time);
+	
+	/**
+	 * Runs this event
+	 */
+	public void activate();
+	
+	/**
+	 * Determines whether this event still has conditions to run
+	 * @return
+	 */
+	public boolean isComplete();
+}
