64f6442087169ae43d404a49fd78da395ecf85ae
VengefulMollusc <ennead_coil@hotmail.com>
2015-10-12 20:54:59 +1300

    More ContainerTests

--- SWEN222 ECS Wars/src/tests/ContainerTests.java
+++ SWEN222 ECS Wars/src/tests/ContainerTests.java
@@ -5,8 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import gameWorld.Controller;
+import gameWorld.Room;
 import gameWorld.SinglePlayerController;
+import gameWorld.characters.DavePlayer;
+import gameWorld.characters.Player;
+import gameWorld.gameObjects.KeyCard;
+import gameWorld.gameObjects.SmallTreasure;
 import gameWorld.gameObjects.Torch;
+import gameWorld.gameObjects.Wall;
 import gameWorld.gameObjects.containers.Cabinet;
 import gameWorld.gameObjects.containers.Container;
 import gameWorld.gameObjects.containers.Pouch;
@@ -88,6 +94,58 @@ public class ContainerTests {
 		// test after attempting to add item while full
 		assertTrue(cabinet.remainingCapacity() == 0);
 		assertTrue(pouch.remainingCapacity() == 0);
+	}
+	
+	@Test
+	/**
+	 * Tests the getItem() method of each container
+	 */
+	public void testGetItem(){
+		cabinet = new Cabinet('f');
+		
+		cabinet.addItem(new Torch());
+		cabinet.addItem(new KeyCard());
+		cabinet.addItem(new SmallTreasure());
+		
+		// test getting items
+		assertTrue(cabinet.getItem(0) instanceof Torch);
+		assertTrue(cabinet.getItem(1) instanceof KeyCard);
+		assertTrue(cabinet.getItem(2) instanceof SmallTreasure);
+		
+		// test that getting an empty item slot returns a blank wall
+		assertTrue(cabinet.getItem(3) instanceof Wall);
+		
+		// test that an index outside the bounds returns a blank Wall
+		assertTrue(cabinet.getItem(-1) instanceof Wall);
+		assertTrue(cabinet.getItem(4) instanceof Wall);
+	}
+	
+	@Test
+	/**
+	 * Tests the pickUpItem() method, where the player picks up an item from the cabinet
+	 */
+	public void testPickUpItem(){
+		cabinet = new Cabinet('f');
+		Controller ctrl = new SinglePlayerController();
+		Player player = new DavePlayer(new Room("Classroom 103", ctrl), 0, 0);
+		
+		cabinet.addItem(new Torch());
+		
+		// test picking up first item
+		cabinet.pickUpItem(0, player);
+		assertTrue(player.inventoryContains(new Torch()) instanceof Torch);
+		
+		cabinet.addItem(new Torch());
+		cabinet.addItem(new Torch());
+		cabinet.addItem(new KeyCard());
+		
+		// Test picking up item with full inventory
+		cabinet.pickUpItem(1, player);
+		cabinet.pickUpItem(0, player);
+		cabinet.pickUpItem(0, player);
+		
+		assertTrue(player.inventoryContains(new KeyCard()) == null);
+		assertTrue(cabinet.getItem(0) instanceof KeyCard);
 	}
 
 }
