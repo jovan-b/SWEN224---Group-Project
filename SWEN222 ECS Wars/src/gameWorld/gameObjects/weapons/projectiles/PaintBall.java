50378a309a77b6b9dce699968a49b7a5af5e5a33
VengefulMollusc <ennead_coil@hotmail.com>
2015-10-14 13:51:34 +1300

    Paintballs now randomise their colour
    between green and yellow

--- SWEN222 ECS Wars/src/gameWorld/gameObjects/weapons/projectiles/PaintBall.java
+++ SWEN222 ECS Wars/src/gameWorld/gameObjects/weapons/projectiles/PaintBall.java
@@ -9,22 +9,25 @@ import javax.imageio.ImageIO;
 import gameWorld.characters.Player;
 
 public class PaintBall extends BasicProjectile{
-	
+
 	private Image image;
 	private Image largeImage;
+	private Image imageOtherColor;
+	private Image largeImageOtherColor;
 	private int bulletSize = 3;
-	
+
 	public PaintBall(){
 		super();
-		int paintballColour = (int) (Math.random()*2);
 		try {
-			image = ImageIO.read(PaintBall.class.getResource("/Projectiles/Paintball"+paintballColour+".png"));
+			image = ImageIO.read(PaintBall.class.getResource("/Projectiles/Paintball0.png"));
 			largeImage = image.getScaledInstance(bulletSize*2, bulletSize*2, Image.SCALE_FAST);
+			imageOtherColor = ImageIO.read(PaintBall.class.getResource("/Projectiles/Paintball1.png"));
+			largeImageOtherColor = imageOtherColor.getScaledInstance(bulletSize*2, bulletSize*2, Image.SCALE_FAST);
 		} catch (IOException e) {
 			System.out.println("Error loading projectile image: " + e.getMessage());
 		}
 	}
-	
+
 	private PaintBall(Player p, int x, int y, double theta, Image image, Image largeImage){
 		super(p, x, y, theta);
 		this.image = image;
@@ -33,7 +36,12 @@ public class PaintBall extends BasicProjectile{
 
 	@Override
 	public Projectile newInstance(Player p, double theta) {
-		return new PaintBall(p, p.getX(), p.getY(), theta, image, largeImage);
+		int randomCol = (int) (Math.random()*2);
+		if (randomCol == 1){
+			return new PaintBall(p, p.getX(), p.getY(), theta, imageOtherColor, largeImageOtherColor);
+		} else {
+			return new PaintBall(p, p.getX(), p.getY(), theta, image, largeImage);
+		}
 	}
 
 	@Override
@@ -49,5 +57,5 @@ public class PaintBall extends BasicProjectile{
 	public int getSize() {
 		return bulletSize;
 	}
-	
+
 }
